b307d24971f5dc777a5360ab12bd6f1d025d2dc6
liyanboss <>
2024-08-11 11:01:13 +0800

    aaa

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+        System.out.println("Hello and welcome!");
+
+        long fdfs = Math.round(10.4);
+        System.out.println(fdfs);
+        fdfs += 10;
+        System.out.println(fdfs);
+
+        long hit2 = Math.round(10.4);
+        System.out.println(hit2);
+        hit2 += 10;
+        System.out.println(hit2);
+
+        //List
+
+
+        int fdf = Math.abs(123);
+        System.out.println(fdf);
+
+        int fdf2 = Math.abs(123);
+        System.out.println(fdf2);
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
